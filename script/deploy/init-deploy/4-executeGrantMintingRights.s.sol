e775e00a0ef18e7d4a6cee50fc6a41b0dd5a7f32
clandestine.eth <96172957+0xClandestine@users.noreply.github.com>
2025-09-23 15:08:34 -0400

    timing

--- script/deploy/init-deploy/4-executeGrantMintingRights.s.sol
+++ script/deploy/init-deploy/4-executeGrantMintingRights.s.sol
@@ -10,11 +10,14 @@ import {Encode} from "eigenlayer-contracts/lib/zeus-templates/src/utils/Encode.s
 
 import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
 import {ITokenHopper} from "src/interfaces/ITokenHopper.sol";
+import {TimeUtils} from "test/utils/TimeUtils.t.sol";
 
 contract ExecuteUpgradeAndSetTimestampSubmitter is SetRewardsPermission {
     using Env for *;
     using Encode for *;
     using ZEnvHelpers for *;
+
+    event ButtonPressed(address indexed caller, uint256 newCooldownHorizon);
 
     function _runAsMultisig() internal virtual override prank(Env.protocolCouncilMultisig()) {
         bytes memory calldata_to_executor = QueueGrantMintingRights._getCalldataToExecutor_queueChanges();
@@ -56,6 +59,33 @@ contract ExecuteUpgradeAndSetTimestampSubmitter is SetRewardsPermission {
 
         vm.expectRevert("TokenHopper.pressButton: call reverted");
         ITokenHopper(OLD_TOKEN_HOPPER).pressButton();
+
+        // Test rewards submission timeline
+        ITokenHopper tokenHopper = ITokenHopper(ZEnvHelpers.state().envAddress("tokenHopper"));
+        
+        // Wed Oct 09 2024 00:00:00 GMT+0000 = 1728432000
+        // Wed Oct 16 2024 00:00:00 GMT+0000 = 1729036800
+        uint256 oct9th2024 = 1728432000;
+        uint256 oct16th2024 = 1729036800;
+        
+        // Verify timestamps using TimeUtils
+        TimeUtils.assertEq(oct9th2024, "Wed Oct 09 2024 00:00:00 GMT+0000");
+        TimeUtils.assertEq(oct16th2024, "Wed Oct 16 2024 00:00:00 GMT+0000");
+        
+        // Warp to October 9th and press button
+        vm.warp(oct9th2024);
+        vm.expectEmit(false, false, false, false, address(tokenHopper));
+        emit ButtonPressed(address(this), oct16th2024); // Cooldown horizon should be Oct 16th
+        tokenHopper.pressButton();
+        
+        // Verify we cannot press again until October 16th
+        vm.expectRevert("TokenHopper.pressButton: button currently unpressable.");
+        tokenHopper.pressButton();
+        
+        // Warp to October 16th and verify we can press again
+        vm.warp(oct16th2024);
+        assertTrue(tokenHopper.canPress(), "Should be able to press on October 16th");
+        tokenHopper.pressButton();
     }
 }
 
