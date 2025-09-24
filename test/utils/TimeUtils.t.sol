2ed551cfcc6adcddffd4cad1e1609ec048ef9747
clandestine.eth <96172957+0xClandestine@users.noreply.github.com>
2025-09-24 14:24:08 -0400

    optimize time utils

--- test/utils/TimeUtils.t.sol
+++ test/utils/TimeUtils.t.sol
@@ -43,30 +43,17 @@ library TimeUtils {
 
     function weekdayString(uint256 timestamp) internal pure returns (string memory) {
         uint256 weekday = DateTimeLib.weekday(timestamp);
-        if (weekday == 1) return "Mon";
-        if (weekday == 2) return "Tue";
-        if (weekday == 3) return "Wed";
-        if (weekday == 4) return "Thu";
-        if (weekday == 5) return "Fri";
-        if (weekday == 6) return "Sat";
-        if (weekday == 7) return "Sun";
-        revert("Invalid weekday");
+        require(weekday >= 1 && weekday <= 7, "Invalid weekday");
+        unchecked {
+            return ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"][weekday - 1]; // checked above
+        }
     }
 
     function monthString(uint256 month) internal pure returns (string memory) {
-        if (month == 1) return "Jan";
-        if (month == 2) return "Feb";
-        if (month == 3) return "Mar";
-        if (month == 4) return "Apr";
-        if (month == 5) return "May";
-        if (month == 6) return "Jun";
-        if (month == 7) return "Jul";
-        if (month == 8) return "Aug";
-        if (month == 9) return "Sep";
-        if (month == 10) return "Oct";
-        if (month == 11) return "Nov";
-        if (month == 12) return "Dec";
-        revert("Invalid month");
+        require(month >= 1 && month <= 12, "Invalid month");
+        unchecked {
+            return ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"][month - 1]; // checked above
+        }
     }
 
     function toPaddedString(uint256 value) internal pure returns (string memory) {
